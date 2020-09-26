f95eae9d3654b99b17244dd873244ba3605f6008
petterastedt <petter.astedt@gmail.com>
2020-09-26 11:51:17 +0200

    First commit

---
+++ js/cards.js
@@ -0,0 +1,50 @@
+const cards = [
+  {
+    "title": "city",
+    "video": "/assets/video/city.mp4",
+    "sounds": [
+      {
+        title: "city sound #1",
+        url: " https://freesound.org/data/previews/214/214319_1175748-lq.mp3"
+      },
+      {
+        title: "city sound #2",
+        url: "https://www.google.com"
+      }
+    ],
+    "textContent": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe quidem quia, quisquam repellendus ut laboriosam deleniti rerum asperiores. Consectetur itaque impedit minus dolorum fugiat?"
+  },
+  {
+    "title": "ocean",
+    "video": "/assets/video/ocean.mp4",
+    "sounds": [
+    {
+      title: "waves crashing #1",
+      url: "https://www.freesoundslibrary.com/wp-content/uploads/2020/01/nature-sounds-ocean-waves-for-relaxation.mp3"
+    },
+    {
+      title: "waves crashing #2",
+      url: "https://www.freesoundslibrary.com/wp-content/uploads/2020/08/applause-sound-effect.mp3"
+    }
+  ],
+    "textContent": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe quidem quia, quisquam repellendus ut laboriosam deleniti rerum asperiores. Consectetur itaque impedit minus dolorum fugiat?"
+  },
+  {
+    "title": "storm",
+    "video": "/assets/video/storm.mp4",
+    "sounds": [{
+      title: "Incoming storm",
+      url: "https://www.freesoundslibrary.com/wp-content/uploads/2019/07/thunderstorm-in-the-city-1.mp3"
+    }],
+    "textContent": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe quidem quia, quisquam repellendus ut laboriosam deleniti rerum asperiores. Consectetur itaque impedit minus dolorum fugiat?"
+  },
+  {
+    "title": "rain",
+    "video": "/assets/video/rain.mp4",
+    "sounds": [{
+      title: "Heavy rain and thunder",
+      url: "https://www.freesoundslibrary.com/wp-content/uploads/2020/08/heavy-rain-and-thunder-sounds-8-minutes.mp3"
+    }],
+    "textContent": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe quidem quia, quisquam repellendus ut laboriosam deleniti rerum asperiores. Consectetur itaque impedit minus dolorum fugiat?"
+  },
+]
---
+++ js/script.js
@@ -0,0 +1,194 @@
+//Run onLoad
+(() => {
+  renderCards()
+  changeVideo(cards.length - 1)
+})()
+
+let isDisabled = false
+
+function renderCards () {
+  const cardList = document.querySelector('.cards-list')
+
+  cards.forEach((item, index) => {
+    const sounds = item.sounds.map(item => `<li class="item-soundItem">${item.title}</li>`)
+
+    const html = `
+      <li class="cards-list-item ${index === cards.length - 1 ? 'active' : ''}" data-category="${item.title}">
+        <div class="item-content boxPadding">
+          <div class="item-content-top">
+            <button class="item-content-buttonVolume" title="mute">
+              <img src="./../assets/svg/volume.svg">
+            </button>
+            <button class="item-content-buttonNext" title="next sound">
+              <img src="./../assets/svg/next.svg">
+            </button>
+            <h2 class="item-content-title">${item.title}</h2>
+            <ul class="item-soundList">
+              ${sounds.join(" ")}
+            </ul>
+          </div>
+          <p class="item-content-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum aliquid similique sed iusto voluptatem nostrum nesciunt neque quis earum, incidunt expedita.</p>
+        </div>
+      </li>
+    `
+    cardList.insertAdjacentHTML('beforeend', html)
+  })
+}
+
+function changeBackground (pos) {
+  const category = cardItems[pos].getAttribute('data-category')
+  const body = document.querySelector('body')
+
+  body.classList.remove(...body.classList)
+  body.classList.add(`theme--${category}`)
+}
+
+function changeVideo (pos) {
+  const overlay = document.querySelector('.overlay')
+  const video = document.querySelector('#video')
+  const videoSource = document.querySelector('video > source')
+
+  video.defaultPlaybackRate = 0.75
+
+  overlay.classList.add('overlay--fade')
+
+  setTimeout(() => {
+    let videoUrl
+
+    if (window.innerWidth >= 1920) {
+      videoUrl = `${cards[pos].video.slice(0,-4)}_1920.mp4`
+    } else if (window.innerWidth > 1024) {
+      videoUrl = `${cards[pos].video.slice(0,-4)}_720.mp4`
+    } else {
+      videoUrl = `${cards[pos].video.slice(0,-4)}_540.mp4`
+    }
+
+    video.pause()
+    video.currentTime = 0
+
+    videoSource.setAttribute('src', videoUrl)
+    video.load()
+    video.play()
+
+    overlay.classList.remove('overlay--fade')
+  }, 1200)
+}
+
+function changeAudio (pos, sound) {
+  const audio = document.querySelector('#audio')
+  const audioSource = document.querySelector('audio > source')
+  let track
+
+  if (sound) {
+    track =  cards[pos].sounds[sound].url
+  } else {
+    track =  cards[pos].sounds[0].url
+  }
+
+  audio.pause()
+  audio.currentTime = 0
+
+  audioSource.setAttribute('src', track)
+  audio.load()
+  audio.play()
+}
+
+// Event listeners
+
+const cardItems = document.querySelectorAll('.cards-list-item')
+const cardInfo = document.querySelector('.cards-info')
+
+let position = cardItems.length - 1
+
+document.body.onkeyup = (e) => {
+  if (e.keyCode == 32 && !isDisabled) {
+    isDisabled = true
+    cardInfo.style.opacity = 0
+
+    cardItems[position].classList.add('cards-list-item--rolledOut')
+    cardItems[position].classList.remove('active')
+
+    position--
+
+    if (position === -1) {
+      const cardList = document.querySelector('.cards-list')
+      cardItems[0].classList.add('cards-list-item--rolledOut')
+      cardList.style.opacity = 0
+
+      setTimeout(() => {
+        cardItems.forEach(item => {
+          item.classList.remove('cards-list-item--rolledOut')
+        })
+      }, 1000)
+
+      setTimeout(() => {
+        cardList.style.opacity = 1
+      }, 1000)
+
+      position = cardItems.length - 1
+    }
+
+    cardItems[position].classList.add('active')
+
+    if (cards[position].video) {
+      changeVideo(position)
+    } else {
+      changeBackground(position)
+    }
+
+    changeAudio(position)
+
+    setTimeout(() => isDisabled = false, 1000)
+  }
+}
+
+cardItems.forEach(item => {
+  const overlay = document.querySelector('.overlay')
+
+  const cardList = document.querySelector('.cards')
+  const list = Array.from(item.children[0].children[0].children[3].children)
+  const buttonVolume = item.children[0].children[0].children[0]
+  const buttonNext = item.children[0].children[0].children[1]
+  let cardIndex = 0
+  let minimized = false
+
+  list.forEach((item, index) => {
+    if (index === 0) {
+      item.classList.add('item-soundItem--active')
+    }
+  })
+
+  buttonNext.addEventListener('click', () => {
+    list[cardIndex].classList.remove('item-soundItem--active')
+
+    if (cardIndex === list.length - 1) {
+      cardIndex = 0
+    } else {
+      cardIndex++
+    }
+
+    list[cardIndex].classList.add('item-soundItem--active')
+
+    changeAudio(position, cardIndex)
+  })
+
+  buttonVolume.addEventListener('click', () => {
+    if (audio.volume === 0) {
+      audio.volume = 1
+    } else {
+      audio.volume = 0
+    }
+
+    buttonVolume.classList.toggle('item-content-buttonVolume--isMuted')
+  })
+
+  overlay.addEventListener('click', () => {
+    if (!minimized) {
+      cardList.classList.remove('cards--isMinimized')
+      minimized = true
+    } else {
+      cardList.classList.add('cards--isMinimized')
+      minimized = false
+    }
+  })
+})
